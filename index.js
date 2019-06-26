1be5e1ae8c0b3cdc869fdcb2dd971c92ff075324
Gabriel Torrecillas Sartori <gabriel.sartori04@gmail.com>
2019-06-26 15:23:15 -0300

    1.0.2

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-module.exports = middleware => async ctx => {
-    const res = await middleware (ctx, { ...ctx.query, ...ctx.params, ...ctx.request.body }, ((ctx.state || {}).user || {}).id)
-    ctx.body
-        = res === true            ? { status: "success" }
-        : res === false           ? { error: "internal error" }
-        : typeof res === "string" ? { error: res }
-        : res
+module.exports = controller => {
+
+    if (controller === undefined)
+        throw new Error ("Controller is undefined")
+
+    return async ctx => {
+
+        const params = { ...ctx.query, ...ctx.params, ...ctx.request.body }
+            , idUser = ((ctx.state || {}).user || {}).id
+            , res    = await controller (ctx, params, idUser)
+
+        ctx.body
+            = res === true            ? { status: "success" }
+            : res === false           ? { error: "internal error" }
+            : typeof res === "string" ? { error: res }
+            : res
+    }
 }

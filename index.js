f1bca815cde441393ba50ec39b3c1c6c54e26349
Gabriel Torrecillas Sartori <gabriel.sartori04@gmail.com>
2019-07-20 16:44:19 -0300

    1.0.7 better error handling

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ module.exports = controller => {
         throw new Error ("Controller is undefined")
 
     return async ctx => {
+        
+        try {
+            
+            const params = { ...ctx.query, ...ctx.params, ...ctx.request.body }
+                , idUser = (ctx.state || {}).user || {}
 
-        const params = { ...ctx.query, ...ctx.params, ...ctx.request.body }
-            , idUser = (ctx.state || {}).user || {}
+            let res = await controller (ctx, params, idUser)
 
-        let res = await controller (ctx, params, idUser)
+            if (Array.isArray (res) && res.length === 2 && Number.isInteger (res[0]) && ! Number.isInteger (res[1])) {
+                ctx.status = res[0]
+                res = res[1]
+            }
 
-        if (Array.isArray (res) && res.length === 2 && Number.isInteger (res[0]) && ! Number.isInteger (res[1])) {
-            ctx.status = res[0]
-            res = res[1]
+            ctx.body
+                = res === true            ? { status: "success" }
+                : res === false           ? { error: "unexpected behaviour" }
+                : typeof res === "string" ? { error: res }
+                : res
         }
-
-        ctx.body
-            = res === true            ? { status: "success" }
-            : res === false           ? { error: "internal error" }
-            : typeof res === "string" ? { error: res }
-            : res
+        catch (e) {
+            console.log (e)
+            ctx.status = 500
+            ctx.body = { error: "internal error" }
+        }
     }
 }
